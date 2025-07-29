ef003de8c65a95f8af8ab7643819105d9b85e6b2
vikkycodes <okorovictor185@gmail.com>
2025-07-29 09:24:56 +0100

    added a link to the github url for redirection

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,41 +1,48 @@
-import { BsBuildings } from "react-icons/bs"
-import { FaLink, FaLocationDot, FaTwitter } from "react-icons/fa6"
+import { BsBuildings } from "react-icons/bs";
+import { FaLink, FaLocationDot, FaTwitter } from "react-icons/fa6";
 
 interface GitHubUser {
   location: string;
-  html_url: string,
-  twitter_username: string,
-  company: string,
+  html_url: string;
+  twitter_username: string;
+  company: string;
 }
 
-const Info = ({ data }:{data: GitHubUser}) => {
+const Info = ({ data }: { data: GitHubUser }) => {
   return (
     <>
-        <div className="flex flex-col lg:flex-row gap-5 lg:gap-5 lg:ml-45 mt-5">
-            <div className="flex flex-col gap-5">
-                <div className="flex items-center gap-2 text-[14px]">
-                    <FaLocationDot />
-                    <p>{data.location ? data.location : "Location not available"}</p>
-                </div>
-                <div className="flex items-center gap-2 text-[14px]">
-                    <FaLink />
-                    <p>{data.html_url ? data.html_url : "Url not available"}</p>
-                </div>
-            </div>
-
-            <div className="flex flex-col gap-5">
-                <div className="flex items-center gap-2 text-[14px]">
-                    <FaTwitter />
-                    <p>@{data.twitter_username ? data.twitter_username : "Twitter handle not available"}</p>
-                </div>
-                <div className="flex items-center gap-2 text-[14px]">
-                    <BsBuildings />
-                    <p>{data.company ? data.company : "Company not available"}</p>
-                </div>
-            </div>
+      <div className="flex flex-col lg:flex-row gap-5 lg:gap-5 lg:ml-45 mt-5">
+        <div className="flex flex-col gap-5">
+          <div className="flex items-center gap-2 text-[14px]">
+            <FaLocationDot />
+            <p>{data.location ? data.location : "Location not available"}</p>
+          </div>
+          <div className="flex items-center gap-2 text-[14px]">
+            <FaLink />
+            <a href={data.html_url} target="_blank">
+              {data.html_url ? data.html_url : "Url not available"}
+            </a>
+          </div>
         </div>
-    </>
-  )
-}
 
-export default Info
+        <div className="flex flex-col gap-5">
+          <div className="flex items-center gap-2 text-[14px]">
+            <FaTwitter />
+            <p>
+              @
+              {data.twitter_username
+                ? data.twitter_username
+                : "Twitter handle not available"}
+            </p>
+          </div>
+          <div className="flex items-center gap-2 text-[14px]">
+            <BsBuildings />
+            <p>{data.company ? data.company : "Company not available"}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Info;
